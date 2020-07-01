90cdd81f05abe8f217d6f30b16f827ff09638fcf
wispamulet <ylyzainali@gmail.com>
2020-07-02 01:23:13 +0800

    add 06

---
+++ 06.ts
@@ -0,0 +1,11 @@
+/*
+* Day 6: Enclose in Brackets
+* In this challenge, you'll:
+
+* Given a string, enclose it in round brackets.
+
+* Example
+* For inputString = "abacaba", the output should be encloseInBrackets(inputString) = "(abacba)".
+*/
+
+const encloseInBrackets = (str: string): string => `(${str})`;
